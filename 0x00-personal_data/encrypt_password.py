d7956b783ac1c4dc1aaf95270750a9ec3de5fff7
willyopera <willy4opera@gmail.com>
2024-04-12 14:27:36 -0400

    Debugged the encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,6 +12,7 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    '''Here, we checked that the hashed password was formed from the given password.
+    '''Here, we checked that the hashed password
+    was formed from the given password.
     '''
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
