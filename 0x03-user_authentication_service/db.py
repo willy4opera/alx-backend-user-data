a98703dce0a14d40d81d1ecb76b4888104e724e1
willyopera <willy4opera@gmail.com>
2024-04-24 15:12:59 -0400

    Added the db.py file. for the add user method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+'''The DB module.
+'''
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    '''The DB class.
+    '''
+
+    def __init__(self) -> None:
+        '''Here, we initialize the new DB instance.
+        '''
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        '''Store in Memory the session object.
+        '''
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        '''Here, we implement the adds a new user to the database.
+        '''
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
