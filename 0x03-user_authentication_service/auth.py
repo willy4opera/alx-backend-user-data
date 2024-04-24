e7e6f1815b627f0b040244d5cce3d70b38e79863
willyopera <willy4opera@gmail.com>
2024-04-24 16:46:25 -0400

    Added the auth.py file. implemented the registered user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,23 @@ def _hash_password(password: str) -> bytes:
     '''Hashes a password.
     '''
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+class Auth:
+    '''Here, we define the
+    auth class to interact with the authentication database.
+    '''
+
+    def __init__(self):
+        '''Here, we initialized the new Auth instance.
+        '''
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''A The adds new user to the database method.
+        '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+    
