e1934ba6ddf888775f383a5e50b60e82e7bfd12e
willyopera <willy4opera@gmail.com>
2024-04-25 11:53:49 -0400

    Updated the app.py file for get user by session ID task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,6 +50,18 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
     return response
 
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieves a user based on a given session ID.
+        """
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
