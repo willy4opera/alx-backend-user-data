87c6c22fbe89c0a87c8f12454f993e3c4d8e0ee9
willyopera <willy4opera@gmail.com>
2024-04-25 07:49:10 -0400

    Added the app.py for The Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,5 +20,20 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    '''POST /users
+    Return:
+        - The account creation payload.
+    '''
+    U_email, U_password = request.form.get(
+        "email"), request.form.get("password")
+    try:
+        AUTH.register_user(U_email, U_password)
+        return jsonify({"email": U_email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
