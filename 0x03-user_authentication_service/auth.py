975e41121944cd68cfdbd38ea8389a46dc3c9f70
willyopera <willy4opera@gmail.com>
2024-04-25 10:12:27 -0400

    Debugged auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,10 +29,25 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        '''A The adds new user to the database method.
+        '''Here, we added new user to the database method.
         '''
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Validate user login details.
+        '''
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
+        except NoResultFound:
+            return False
+        return False
